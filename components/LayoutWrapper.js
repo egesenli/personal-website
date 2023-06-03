42899d1dc8f1b9262af9632f8cf0a794baf17b49
egesenli <egesenli@gmail.com>
2023-06-03 19:09:14 -0400

    feat: integrate Navbar component into LayoutWrapper

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,11 +1,11 @@
 import siteMetadata from '@/data/siteMetadata'
-import headerNavLinks from '@/data/headerNavLinks'
 import Logo from '@/data/logo.svg'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
+import Navbar from './Navbar'
 
 const LayoutWrapper = ({ children }) => {
   return (
@@ -29,17 +29,7 @@ const LayoutWrapper = ({ children }) => {
             </Link>
           </div>
           <div className="flex items-center text-base leading-5">
-            <div className="hidden sm:block">
-              {headerNavLinks.map((link) => (
-                <Link
-                  key={link.title}
-                  href={link.href}
-                  className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
-                >
-                  {link.title}
-                </Link>
-              ))}
-            </div>
+            <Navbar />
             <ThemeSwitch />
             <MobileNav />
           </div>
