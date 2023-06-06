a8a7604289b27cd58f5b4aca33025c7638cbfa1e
egesenli <egesenli@gmail.com>
2023-06-06 10:51:02 -0400

    feat(components): create Navbar component and refactor LayoutWrapper

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,43 +1,16 @@
-import siteMetadata from '@/data/siteMetadata'
-import Logo from '@/data/logo.svg'
-import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
-import MobileNav from './MobileNav'
-import ThemeSwitch from './ThemeSwitch'
 import Navbar from './Navbar'
 
 const LayoutWrapper = ({ children }) => {
   return (
-    <SectionContainer>
-      <div className="flex h-screen flex-col justify-between">
-        <header className="flex items-center justify-between py-10">
-          <div>
-            <Link href="/" aria-label={siteMetadata.headerTitle}>
-              <div className="flex items-center justify-between">
-                <div className="mr-3">
-                  <Logo />
-                </div>
-                {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
-                    {siteMetadata.headerTitle}
-                  </div>
-                ) : (
-                  siteMetadata.headerTitle
-                )}
-              </div>
-            </Link>
-          </div>
-          <div className="flex items-center text-base leading-5">
-            <Navbar />
-            <ThemeSwitch />
-            <MobileNav />
-          </div>
-        </header>
-        <main className="mb-auto">{children}</main>
-        <Footer />
-      </div>
-    </SectionContainer>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <SectionContainer>
+        <main className="flex-grow">{children}</main>
+      </SectionContainer>
+      <Footer />
+    </div>
   )
 }
 
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,54 @@
+import React from 'react'
+import Image from 'next/image'
 import Link from './Link'
 import headerNavLinks from '@/data/headerNavLinks'
+import siteMetadata from '@/data/siteMetadata'
+import ThemeSwitch from './ThemeSwitch'
+import MobileNav from './MobileNav'
+import logoWhite from '/public/static/images/logo-white.png'
+import logoBlack from '/public/static/images/logo-black.png'
+import { useTheme } from 'next-themes'
 
-const Navbar = () => (
-  <nav className="hidden sm:block">
-    {headerNavLinks.map((link) => (
-      <Link
-        key={link.title}
-        href={link.href}
-        className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
-      >
-        {link.title}
-      </Link>
-    ))}
-  </nav>
-)
+const Navbar = () => {
+  const { theme, resolvedTheme } = useTheme()
+
+  return (
+    <nav>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center ml-4 mt-2">
+          <Link href="/" aria-label={siteMetadata.headerTitle}>
+            <div className="flex items-center">
+              <div className="mr-3">
+                <Image
+                  src={theme === 'dark' || resolvedTheme === 'dark' ? logoWhite : logoBlack}
+                  alt="Logo"
+                  width={100}
+                  height={75}
+                />
+              </div>
+            </div>
+          </Link>
+        </div>
+        <div className="flex items-center justify-center mt-2">
+          <div className="sm:flex hidden space-x-4">
+            {headerNavLinks.map((link) => (
+              <Link
+                key={link.title}
+                href={link.href}
+                className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
+              >
+                {link.title}
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className="flex items-center space-x-4 mt-2 mr-4">
+          <MobileNav />
+          <ThemeSwitch />
+        </div>
+      </div>
+    </nav>
+  )
+}
 
 export default Navbar
