44173b6aa6dca12d26b2d2d27eb5656180be93f7
egesenli <egesenli@gmail.com>
2023-06-06 16:40:28 -0400

    refactor: Update LayoutWrapper component with MobileNav functionality

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,11 +1,20 @@
+import React, { useState } from 'react'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import Navbar from './Navbar'
+import MobileNav from './MobileNav'
 
 const LayoutWrapper = ({ children }) => {
+  const [navShow, setNavShow] = useState(false)
+
+  const onToggleNav = () => {
+    setNavShow((prevStatus) => !prevStatus)
+  }
+
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar />
+      <Navbar onToggleNav={onToggleNav} />
+      <MobileNav navShow={navShow} onToggleNav={onToggleNav} />
       <SectionContainer>
         <main className="flex-grow">{children}</main>
       </SectionContainer>
