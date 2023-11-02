9bf117335f1c1175b14dba416019fd97be9953a5
egesenli <egesenli@gmail.com>
2023-11-02 17:21:22 -0400

    style: Remove top margin to a container in the Music component

--- pages/music.js
+++ pages/music.js
@@ -79,7 +79,7 @@ export default function Music() {
           </div>
         )}
 
-        <div className="space-y-6">
+        <div className="mt-6 space-y-6">
           <div>
             <Iframe
               url="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/744434260&color=%2344444c&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
