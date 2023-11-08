4ff374cc620fd8eeb7f623fa4567d46d17c764cf
egesenli <egesenli@gmail.com>
2023-11-08 03:39:44 -0500

    refactor(author-layout): Update AuthorLayout component

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -1,31 +1,29 @@
-import SocialIcon from '@/components/social-icons'
+import SocialIcon from '@/components/social-icons' // Import your SocialIcon component
 import Image from '@/components/Image'
 import { PageSEO } from '@/components/SEO'
 
 export default function AuthorLayout({ children, frontMatter }) {
-  const { name, avatar, occupation, company, email, twitter, linkedin, github } = frontMatter
+  const { name, avatar, occupation, email, twitter, linkedin, github } = frontMatter
 
   return (
     <>
       <PageSEO title={`About - ${name}`} description={`About me - ${name}`} />
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            About
-          </h1>
-        </div>
-        <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
-          <div className="flex flex-col items-center pt-8">
+      <div className="pt-8">
+        <div className="mb-8 flex flex-col-reverse items-center justify-between sm:flex-row sm:items-center">
+          <div className="pt-8 text-center sm:text-left">
+            <h1 className="text-xl font-bold md:text-3xl lg:text-4xl">{name}</h1>
+            <h2 className="text-sm font-normal md:text-base">
+              {occupation} <span className="font-semibold"></span>
+            </h2>
+          </div>
+          <div>
             <Image
-              src={avatar}
-              alt="avatar"
-              width="192px"
-              height="192px"
-              className="h-48 w-48 rounded-full"
+              alt={name}
+              height={140}
+              width={140}
+              src={avatar || ''}
+              className="rounded-full object-cover grayscale"
             />
-            <h3 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
-            <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
-            <div className="text-gray-500 dark:text-gray-400">{company}</div>
             <div className="flex space-x-3 pt-6">
               <SocialIcon kind="mail" href={`mailto:${email}`} />
               <SocialIcon kind="github" href={github} />
@@ -33,7 +31,9 @@ export default function AuthorLayout({ children, frontMatter }) {
               <SocialIcon kind="twitter" href={twitter} />
             </div>
           </div>
-          <div className="prose max-w-none pb-8 pt-8 dark:prose-dark xl:col-span-2">{children}</div>
+        </div>
+        <div className="prose max-w-none pb-8 text-justify text-sm dark:prose-dark md:text-lg xl:col-span-2">
+          {children}
         </div>
       </div>
     </>
