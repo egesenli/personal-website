7f4b1e60bd9eb453bcfdc85b383209eb7984f252
egesenli <egesenli@gmail.com>
2024-01-16 20:02:56 -0500

    chore: update site metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,6 +1,6 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
+  title: 'Erkan Ege Senli Blog',
+  author: 'Erkan Ege Senli',
   headerTitle: 'Erkan Ege Senli',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
