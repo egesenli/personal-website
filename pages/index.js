ae998b48e1adcb5e5039b26da76ac7fe5ecffe89
egesenli <egesenli@gmail.com>
2024-02-02 03:19:40 -0500

    refactor: Remove newsletter component from homepage

--- pages/index.js
+++ pages/index.js
@@ -202,11 +202,6 @@ export default function Home({ posts }) {
           )}
         </div>
       </div>
-      {siteMetadata.newsletter.provider !== '' && (
-        <div className="flex items-center justify-center pt-4">
-          <NewsletterForm />
-        </div>
-      )}
     </>
   )
 }
