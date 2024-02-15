1113d055c34fa1dd8524421a22cf2daeb64f4fc9
egesenli <egesenli@gmail.com>
2024-02-15 00:59:58 -0500

    style: Fix minor styling issue for divider

--- pages/music.js
+++ pages/music.js
@@ -74,7 +74,7 @@ export default function Music() {
   return (
     <>
       <PageSEO title="Music" description={siteMetadata.description} />
-      <div className="divide-y divide-gray-700">
+      <div className="divide-y divide-gray-400">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Music
